1ec195771c8faeb8fae6e90b81205491558c7cc1
sandeepg080808 <sandeepg080808@gmail.com>
2023-06-28 18:13:22 +0530

    Chapter 19 : Parameter and Argument

---
+++ js/chapter19.js
@@ -0,0 +1,15 @@
+// here num is parameter 
+function multiply(num){
+    document
+    for (i =0;i<11;i++){
+        document.write(`${num} * ${i} = ${num* i}`);
+        document.write("<br>")
+    }
+    
+}
+// here 2,3,4 are argument  
+multiply(2);
+document.write("<br>")
+multiply(3);
+document.write("<br>")
+multiply(4);
